57143c2714bd078e2c3754b16162ca0fc6dc884b
Benjamin <bhollon@duti.site>
2019-01-06 20:28:23 +0800

    Stop flashing notifications and change style

--- mainChat/chat_inPage.js
+++ mainChat/chat_inPage.js
@@ -184,7 +184,7 @@ lookForApiKeys(0);
         success: function(response) {
           var data = response.data;
           var notifications = response.notifications;
-          processNotifications(notifications);
+          if (notifications && notifications != globalNotifications) processNotifications(notifications);
           if (config['messagecount']) {
             data["chat"] = data["chat"].slice(0, config['messagecount']);
           }
@@ -271,7 +271,7 @@ lookForApiKeys(0);
         success: function(response) {
           var data = response.data;
           var notifications = response.notifications;
-          processNotifications(notifications);
+          if (notifications && notifications != globalNotifications) processNotifications(notifications);
           if (config['messagecount'] >=1 && config['messagecount'] <= 199) {
             data = data.slice(0, config['messagecount']);
           }
@@ -762,7 +762,7 @@ lookForApiKeys(0);
       var data = response.data;
       setPartyId(data['party']['_id']);
       var notifications = response.notifications;
-      processNotifications(notifications);
+      if (notifications && notifications != globalNotifications) processNotifications(notifications);
       setContributorTier(data['contributor']['level']);
       setHeroName(data['auth']['local']['username']);
       if (!data['party']['_id']) {
@@ -782,7 +782,7 @@ lookForApiKeys(0);
       headers: apiHeaders,
       success: function(response) {
         var notifications = response.notifications;
-        processNotifications(notifications);
+        if (notifications && notifications != globalNotifications) processNotifications(notifications);
       }
     });
   }, 20000);
